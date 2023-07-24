45a46ce3d8bd9add807a36223e94c2f92db329db
Adam Shalala <adamshalala@192-168-1-70.tpgi.com.au>
2023-07-24 19:47:06 +1000

    basic placeholder header and footer

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="p-3 bg-dark text-white">
+      <h1>Warehouse Management</h1>
+    </header>
+  );
+}
+
+export default Header;
