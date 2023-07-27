a898d7f841619b5d3804a098f4145d57cb930601
Adam Shalala <adamshalala@192-168-1-70.tpgi.com.au>
2023-07-27 15:20:03 +1000

    updated app.js so after login, redirected to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+
+
+import React, { useState, useEffect } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -13,23 +13,14 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import StaffManagement from './pages/StaffManagement';
 
-import AuthService from './utils/auth'; // Import the AuthService
+import AuthService from './utils/auth';
 
-
-
-
-// Construct our main GraphQL API endpoint
-// Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
 
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -39,27 +30,68 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
-  const isAuthenticated = AuthService.loggedIn();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setIsAuthenticated(AuthService.loggedIn());
+  }, []);
+
+  const handleLogin = (token) => {
+    // Save the token to local storage or use your AuthService implementation
+    localStorage.setItem('id_token', token);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    // Remove the token from local storage or use your AuthService implementation
+    localStorage.removeItem('id_token');
+    setIsAuthenticated(false);
+  };
 
   return (
     <ApolloProvider client={client}>
       <Router>
         <div className="App">
-          <Header />
+          <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />
         
           <Routes>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
-<Route path="/staffmanagement" element={isAuthenticated ? <StaffManagement /> : <Navigate to="/login" />} />
+            <Route
+              path="/signup"
+              element={<Signup onLogin={handleLogin} />}
+            />
+            <Route
+              path="/login"
+              element={
+                isAuthenticated ? (
+                  <Navigate to="/dashboard" />
+                ) : (
+                  <Login onLogin={handleLogin} />
+                )
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                isAuthenticated ? <Dashboard /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/staffmanagement"
+              element={
+                isAuthenticated ? (
+                  <StaffManagement />
+                ) : (
+                  <Navigate to="/login" />
+                )
+              }
+            />
           </Routes>
-          <Footer/> 
+          <Footer />
         </div>
       </Router>
     </ApolloProvider>
