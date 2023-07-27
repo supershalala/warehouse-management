17a7b6a80442ad0b9c29f0af730395332381fd52
Adam Shalala <adamshalala@192-168-1-70.tpgi.com.au>
2023-07-27 20:24:53 +1000

    updated files for heroky deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,111 @@
 
 
+// import React, { useState, useEffect } from 'react';
+// import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// import Signup from './pages/Signup';
+// import Login from './pages/Login';
+// import Dashboard from './pages/Dashboard';
+// import StaffManagement from './pages/StaffManagement';
+
+// import HomePage from './pages/Homepage';
+
+// import AuthService from './utils/auth';
+
+
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:3001/graphql',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+//   useEffect(() => {
+//     setIsAuthenticated(AuthService.loggedIn());
+//   }, []);
+
+//   const handleLogin = (token) => {
+//     // Save the token to local storage or use your AuthService implementation
+//     localStorage.setItem('id_token', token);
+//     setIsAuthenticated(true);
+//   };
+
+//   const handleLogout = () => {
+//     // Remove the token from local storage or use your AuthService implementation
+//     localStorage.removeItem('id_token');
+//     setIsAuthenticated(false);
+//   };
+
+//   return (
+//     <ApolloProvider client={client}>
+//       <Router>
+//         <div className="App">
+//           <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />
+        
+//           <Routes>
+
+//           <Route path="/" element={<HomePage />} />
+
+//             <Route
+//               path="/signup"
+//               element={<Signup onLogin={handleLogin} />}
+//             />
+//             <Route
+//               path="/login"
+//               element={
+//                 isAuthenticated ? (
+//                   <Navigate to="/dashboard" />
+//                 ) : (
+//                   <Login onLogin={handleLogin} />
+//                 )
+//               }
+//             />
+//             <Route
+//               path="/dashboard"
+//               element={
+//                 isAuthenticated ? <Dashboard /> : <Navigate to="/login" />
+//               }
+//             />
+//             <Route
+//               path="/staffmanagement"
+//               element={
+//                 isAuthenticated ? (
+//                   <StaffManagement />
+//                 ) : (
+//                   <Navigate to="/login" />
+//                 )
+//               }
+//             />
+//           </Routes>
+//           <Footer />
+//         </div>
+//       </Router>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
+
 import React, { useState, useEffect } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
@@ -17,9 +123,16 @@ import HomePage from './pages/Homepage';
 
 import AuthService from './utils/auth';
 
+let uri;
+
+if (process.env.NODE_ENV === 'production') {
+  uri = '/graphql';
+} else {
+  uri = 'http://localhost:3001/graphql';
+}
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: uri,
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -45,13 +158,11 @@ function App() {
   }, []);
 
   const handleLogin = (token) => {
-    // Save the token to local storage or use your AuthService implementation
     localStorage.setItem('id_token', token);
     setIsAuthenticated(true);
   };
 
   const handleLogout = () => {
-    // Remove the token from local storage or use your AuthService implementation
     localStorage.removeItem('id_token');
     setIsAuthenticated(false);
   };
@@ -61,15 +172,9 @@ function App() {
       <Router>
         <div className="App">
           <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />
-        
           <Routes>
-
-          <Route path="/" element={<HomePage />} />
-
-            <Route
-              path="/signup"
-              element={<Signup onLogin={handleLogin} />}
-            />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<Signup onLogin={handleLogin} />} />
             <Route
               path="/login"
               element={
