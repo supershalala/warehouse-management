8784768d62dc05f46fe67fe1cde179b3e7bd911e
Adam Shalala <adamshalala@192-168-1-70.tpgi.com.au>
2023-07-27 15:06:46 +1000

    fixed navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,9 @@
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AuthService from '../../utils/auth'; 
+import { Navbar, Nav, Button, Container, Row, Col } from 'react-bootstrap';
 
 function Header() {
   const isAuthenticated = AuthService.loggedIn();
@@ -10,25 +13,35 @@ function Header() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <Link className="navbar-brand col-4 pl-0 mr-0" to="/">Warehouse Management</Link>
-      <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navigations-09" aria-expanded="false">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="navbar-collapse justify-content-md-center col-8 px-0 collapse" id="navigations-09">
-      <ul className="navbar-nav col-6 justify-content-center">
-          {isAuthenticated && <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">Dashboard</Link>
-            <Link className="nav-link" to="/staffmanagement">Staff Management</Link>
-          </li>}
-        </ul>
-        <div className="col-6 text-lg-right px-0">
-          {!isAuthenticated && <Link className="btn btn-outline-primary mt-2 mt-lg-0 mr-0" to="/signup">Sign up</Link>}
-          {!isAuthenticated && <Link className="btn btn-outline-primary mt-2 mt-lg-0 mr-0 ml-2" to="/login">Login</Link>}
-          {isAuthenticated && <button className="btn btn-outline-primary mt-2 mt-lg-0 mr-0 ml-2" onClick={handleLogout}>Logout</button>}
-        </div>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container fluid>
+        <Row className="w-100">
+          <Col className="pr-0">
+            <Navbar.Brand as={Link} to="/">Warehouse Management</Navbar.Brand>
+          </Col>
+          <Col className="px-0">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="ml-auto" />
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+              {isAuthenticated && 
+              <Nav className="justify-content-center">
+                <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                <Nav.Link as={Link} to="/staffmanagement">Staff Management</Nav.Link>
+              </Nav>}
+              <Nav>
+                {!isAuthenticated &&
+                <>
+                  <Nav.Link as={Link} to="/signup">Sign up</Nav.Link>
+                  <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                </>}
+                {isAuthenticated &&
+                  <Button variant="outline-light" onClick={handleLogout}>Logout</Button>
+                }
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
+      </Container>
+    </Navbar>
   );
 }
 
