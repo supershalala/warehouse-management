98f3218184b6a4076dd44c2aa7bbe48ca920beba
Adam Shalala <adamshalala@192-168-1-70.tpgi.com.au>
2023-07-27 14:30:26 +1000

    added verification to the signup screen

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { SIGNUP_USER } from '../utils/mutations';
@@ -5,6 +6,12 @@ import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [formState, setFormState] = useState({
+    name: '',
+    phone: '+61',
+    password: '',
+  });
+
+  const [validationErrors, setValidationErrors] = useState({
     name: '',
     phone: '',
     password: '',
@@ -14,17 +21,48 @@ const SignUp = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    try {
-      // Set the role value to "manager"
-      formState.role = 'manager';
 
-      const { data } = await signUp({
-        variables: { ...formState }
-      });
+    // Perform final validation before submitting the form
+    let isValid = true;
+    const errors = {
+      name: '',
+      phone: '',
+      password: '',
+    };
 
-      console.log(data);
-    } catch (e) {
-      console.error(e);
+    if (!formState.name.trim()) {
+      isValid = false;
+      errors.name = 'Name is required';
+    } else if (!/^[a-zA-Z]+$/.test(formState.name.trim())) {
+      isValid = false;
+      errors.name = 'Name should contain only letters';
+    }
+
+    if (!/^\+61\d{9}$/.test(formState.phone.trim())) {
+      isValid = false;
+      errors.phone = 'Phone number must be in the format +61XXXXXXXXX';
+    }
+
+    if (formState.password.length < 10 || !/\d/.test(formState.password) || !/[a-zA-Z]/.test(formState.password)) {
+      isValid = false;
+      errors.password = 'Password must be at least 10 characters and contain a mixture of numbers and characters';
+    }
+
+    setValidationErrors(errors);
+
+    if (isValid) {
+      try {
+        // Set the role value to "manager"
+        formState.role = 'manager';
+
+        const { data } = await signUp({
+          variables: { ...formState }
+        });
+
+        console.log(data);
+      } catch (e) {
+        console.error(e);
+      }
     }
   };
 
@@ -34,6 +72,47 @@ const SignUp = () => {
       ...formState,
       [name]: value,
     });
+  };
+
+  const handleBlur = event => {
+    const { name } = event.target;
+    let isValid = true;
+    const errors = {
+      name: '',
+      phone: '',
+      password: '',
+    };
+
+    if (name === 'name') {
+      if (!formState.name.trim()) {
+        isValid = false;
+        errors.name = 'Name is required';
+      } else if (!/^[a-zA-Z]+$/.test(formState.name.trim())) {
+        isValid = false;
+        errors.name = 'First Name should only be one word and contain only letters';
+      }
+    }
+
+    if (name === 'phone') {
+      if (!/^\+61\d{9}$/.test(formState.phone.trim())) {
+        isValid = false;
+        errors.phone = 'Phone number must be in the format +61XXXXXXXXX';
+      }
+    }
+
+    if (name === 'password') {
+      if (formState.password.length < 10 || !/\d/.test(formState.password) || !/[a-zA-Z]/.test(formState.password)) {
+        isValid = false;
+        errors.password = 'Password must be at least 10 characters and contain a mixture of numbers and characters';
+      }
+    }
+
+    if (!isValid) {
+      setValidationErrors({
+        ...validationErrors,
+        [name]: errors[name],
+      });
+    }
   };
 
   return (
@@ -51,24 +130,21 @@ const SignUp = () => {
           ) : (
             <form onSubmit={handleFormSubmit}>
               <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" className="form-control" name="name" onChange={handleChange} />
+                <label htmlFor="name">First Name</label>
+                <input type="text" id="name" className={`form-control ${validationErrors.name ? 'is-invalid' : ''}`} name="name" onChange={handleChange} onBlur={handleBlur} />
+                {validationErrors.name && <div className="invalid-feedback">{validationErrors.name}</div>}
               </div>
-
-              {/* Remove the "role" form group */}
-              {/* <div className="form-group">
-                <label htmlFor="role">Role</label>
-                <input type="text" id="role" className="form-control" name="role" onChange={handleChange} />
-              </div> */}
 
               <div className="form-group">
                 <label htmlFor="phone">Phone</label>
-                <input type="text" id="phone" className="form-control" name="phone" onChange={handleChange} />
+                <input type="text" id="phone" className={`form-control ${validationErrors.phone ? 'is-invalid' : ''}`} name="phone" value={formState.phone} onChange={handleChange} onBlur={handleBlur} />
+                {validationErrors.phone && <div className="invalid-feedback">{validationErrors.phone}</div>}
               </div>
 
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" className="form-control" name="password" onChange={handleChange} />
+                <input type="password" id="password" className={`form-control ${validationErrors.password ? 'is-invalid' : ''}`} name="password" onChange={handleChange} onBlur={handleBlur} />
+                {validationErrors.password && <div className="invalid-feedback">{validationErrors.password}</div>}
               </div>
 
               <button type="submit" className="btn btn-primary">
